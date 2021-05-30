9c17eca65589fa01baae656791869fce8c14831b
Laura7890 <vanegasaleja697@gmail.com>
2021-05-29 19:34:25 -0500

    consultrando datos de la tabla usuario

---
+++ ProyectoASPWEB/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+    }
+}
