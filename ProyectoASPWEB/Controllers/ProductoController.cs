543f216d94fcccac74907ca3b00d7faf361ca331
Laura7890 <vanegasaleja697@gmail.com>
2021-05-30 14:33:51 -0500

    Creando las vista parcial

---
+++ ProyectoASPWEB/Controllers/ProductoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.producto.ToList());
+            }
+        }
+
+        public static string NombreProveedor(int idProveedor)
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+        public ActionResult ListarProveedores()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return PartialView(db.proveedor.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
