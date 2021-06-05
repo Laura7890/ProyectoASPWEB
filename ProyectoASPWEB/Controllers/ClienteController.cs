73137b5a53548b45f8c3769d56a529d86929ce1d
Laura7890 <vanegasaleja697@gmail.com>
2021-06-04 22:24:10 -0500

    Creando el CRUD de la tabla usuario, "Leer"

---
+++ ProyectoASPWEB/Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+    }
+}
