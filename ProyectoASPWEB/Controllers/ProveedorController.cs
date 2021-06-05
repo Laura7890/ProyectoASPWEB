fda9810be003b275c89ce71e387da5c8a4e0eb85
Laura7890 <vanegasaleja697@gmail.com>
2021-06-04 23:59:00 -0500

    Creando el crud de la tabla proveedores.

---
+++ ProyectoASPWEB/Controllers/ProveedorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.proveedor.ToList());
+            }
+        }
+    }
+}
