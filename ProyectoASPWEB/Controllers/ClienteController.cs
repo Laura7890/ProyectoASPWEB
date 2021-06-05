5c30223acd91955a4a402ccdea9926d9ccd1d179
Laura7890 <vanegasaleja697@gmail.com>
2021-06-04 23:29:51 -0500

    Creando el "Edit" de la tabla cliente.

--- ProyectoASPWEB/Controllers/ClienteController.cs
+++ ProyectoASPWEB/Controllers/ClienteController.cs
@@ -28,7 +28,6 @@ namespace ProyectoASPWEB.Controllers
         [ValidateAntiForgeryToken]
 
         public ActionResult Create(cliente cliente)
-
         {
             if (!ModelState.IsValid)
                 return View();
@@ -47,7 +46,60 @@ namespace ProyectoASPWEB.Controllers
                 ModelState.AddModelError("", "error" + ex);
                 return View();
             }
+        }
 
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    cliente finduser = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(finduser);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                var cliente = db.cliente.Find(id);
+                db.cliente.Remove(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente clienteEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    cliente user = db.cliente.Find(clienteEdit.id);
+
+                    user.nombre = clienteEdit.nombre;
+                    user.documento = clienteEdit.documento;
+                    user.email = clienteEdit.email;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
 
         }
     }
