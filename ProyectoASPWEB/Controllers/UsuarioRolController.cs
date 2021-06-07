5f31d1ab98eb4ec964fb47b9ccce229b20b0869e
Laura7890 <vanegasaleja697@gmail.com>
2021-06-07 09:38:51 -0500

    "Read" de la tabla UsuarioRol.

--- ProyectoASPWEB/Controllers/UsuarioRolController.cs
+++ ProyectoASPWEB/Controllers/UsuarioRolController.cs
@@ -7,16 +7,15 @@ using ProyectoASPWEB.Models;
 
 namespace ProyectoASPWEB.Controllers
 {
-    public class UsuarioRolesController : Controller
+    public class UsuarioRolController : Controller
     {
         // GET: UsuarioRol
         public ActionResult Index()
         {
-            using (var db = new  inventario2021_2Entities())
+            using (var db = new inventario2021_2Entities())
             {
                 return View(db.usuariorol.ToList());
             }
-
         }
 
         public static string NombreUsuarioRol(int idUsuarioRol)
