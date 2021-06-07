56a497e65d18b68b34289ee1ee873ebaa10e610a
Laura7890 <vanegasaleja697@gmail.com>
2021-06-07 08:22:44 -0500

    Creando el "CRUD" de la tabla Rolles.

---
+++ ProyectoASPWEB/Controllers/RolesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.roles.ToList());
+            }
+
+        }
+
+    }
+}
