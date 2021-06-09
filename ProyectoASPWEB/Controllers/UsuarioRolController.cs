c509c0a57866500f66978c5243b6892324538070
Laura7890 <vanegasaleja697@gmail.com>
2021-06-08 19:45:50 -0500

    Creando el "Edit" de la tabla UsuarioRol.

--- ProyectoASPWEB/Controllers/UsuarioRolController.cs
+++ ProyectoASPWEB/Controllers/UsuarioRolController.cs
@@ -79,5 +79,49 @@ namespace ProyectoASPWEB.Controllers
             }
         }
 
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    usuariorol finduser = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                    return View(finduser);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuariorol usuarioRolEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    usuariorol usuarioRol = db.usuariorol.Find(usuarioRolEdit.id);
+
+                    usuarioRol.idUsuario = usuarioRolEdit.idUsuario;
+                    usuarioRol.idRol = usuarioRolEdit.idRol;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
     }
 }
