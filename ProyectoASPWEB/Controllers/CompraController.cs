f2552c0910e4a3c6691a64a5c9e4dfa8bf0335ed
Laura7890 <vanegasaleja697@gmail.com>
2021-06-08 21:08:50 -0500

    Creando el "Create" de la tabla Compra.

--- ProyectoASPWEB/Controllers/CompraController.cs
+++ ProyectoASPWEB/Controllers/CompraController.cs
@@ -26,11 +26,55 @@ namespace ProyectoASPWEB.Controllers
             }
         }
 
+        public ActionResult ListarUsuario()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
         public static string NombreCliente(int idCliente)
         {
             using (var db = new inventario2021_2Entities())
             {
                 return db.cliente.Find(idCliente).nombre;
+            }
+        }
+
+        public ActionResult ListarCliente()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return PartialView(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(compra Compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    db.compra.Add(Compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
             }
         }
     }
