e1757453d3d8870f8cd562a575a4df35ba7ad5a0
Laura7890 <vanegasaleja697@gmail.com>
2021-06-08 19:32:24 -0500

    Correccion de los CRUD de la tabla "UsuarioRol" y Compra.

---
+++ ProyectoASPWEB/Controllers/CompraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.compra.ToList());
+            }
+        }
+
+        public static string NombreUsuario(int idUsuario)
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+    }
+}
--- ProyectoASPWEB/Controllers/UsuarioRolController.cs
+++ ProyectoASPWEB/Controllers/UsuarioRolController.cs
@@ -51,9 +51,33 @@ namespace ProyectoASPWEB.Controllers
         }
 
         public ActionResult Create()
-
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(usuariorol usuariorol)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    db.usuariorol.Add(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
     }
 }
