414fa1117fe0fee6990a7cefc9ee3f03232bdf82
Laura7890 <vanegasaleja697@gmail.com>
2021-06-09 22:28:43 -0500

    Terminando el "Create" de la tabla producto compra y organizando los Id.

--- ProyectoASPWEB/Controllers/Producto_CompraController.cs
+++ ProyectoASPWEB/Controllers/Producto_CompraController.cs
@@ -26,11 +26,55 @@ namespace ProyectoASPWEB.Controllers
             }
         }
 
+        public ActionResult ListarCompra()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return PartialView(db.compra.ToList());
+            }
+        }
+
         public static string NombreProducto(int idProducto)
         {
             using (var db = new inventario2021_2Entities())
             {
                 return db.producto.Find(idProducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProducto()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return PartialView(db.producto.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto_compra ProductoCompra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021_2Entities())
+                {
+                    db.producto_compra.Add(ProductoCompra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
             }
         }
     }
