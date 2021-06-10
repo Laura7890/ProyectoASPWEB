520c24223fcc758a9e9c44f0a92bffd802eb13a6
Laura7890 <vanegasaleja697@gmail.com>
2021-06-09 22:57:00 -0500

    Creando el CRUD de la tabla Producto_Imagen y realizando el "read".

---
+++ ProyectoASPWEB/Controllers/Producto_ImagenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPWEB.Models;
+
+namespace ProyectoASPWEB.Controllers
+{
+    public class Producto_ImagenController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021_2Entities())
+            {
+                return View(db.producto_imagen.ToList());
+            }
+        }
+    }
+}
